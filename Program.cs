ed5350f2136a59fb428cdbfbd5fc37a1d78a949f
Ultimitepower36 <113638764+Ultimitepower36@users.noreply.github.com>
2022-11-01 14:02:02 -0600

    Setup Work
    Setting up the project for building

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+
+
+// dotnet add package Raylib-cs --version 4.2.0.1
+
+namespace HelloWorld
+{
+    static class Program
+    {
+        public static void Main()
+        {
+
+            var ScreenHeight = 480;
+            var ScreenWidth = 800;
+            var BallPosition = new Vector2(ScreenHeight / 2, ScreenWidth / 2);
+            var BallMovementSpeed = 1;
+            float BallRadius = 50;
+
+            Raylib.InitWindow(ScreenHeight, ScreenWidth, "Ball");
+            Raylib.SetTargetFPS(60);
+
+            while (!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.WHITE);
+
+                Raylib.DrawText("Move the ball with the arrow keys!", 12, 12, 20, Color.BLACK);
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT)) {
+                    BallPosition.X += BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT)) {
+                    BallPosition.X -= BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP)) {
+                    BallPosition.Y -= BallMovementSpeed;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN)) {
+                    BallPosition.Y  += BallMovementSpeed;
+                }
+
+                Raylib.DrawCircleV(BallPosition, BallRadius, Color.MAROON);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
